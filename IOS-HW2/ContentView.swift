c7a92c16082418927bedb8a2c8d948e8417d79f5
rong1002 <borong1002@gmail.com>
2020-10-08 13:40:11 +0800

    Initial Commit

---
+++ IOS-HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS-HW2
+//
+//  Created by Lin Bo Rong on 2020/10/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS-HW2/IOS_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW2App.swift
+//  IOS-HW2
+//
+//  Created by Lin Bo Rong on 2020/10/8.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
